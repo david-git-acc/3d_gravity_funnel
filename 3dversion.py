57dd5d6595839628087ba8fbb7c43fc5695022cc
gamma82 <davidlitchfield.work@gmail.com>
2023-10-29 10:46:59 +0000

    Modularised the wall collision code and made some adjustments

--- 3dversion.py
+++ 3dversion.py
@@ -3,20 +3,21 @@ from matplotlib.animation import FuncAnimation
 import numpy as np
 from numpy.ma import masked_where
 
-position = np.array([2+3j, 3+3.8j, 2+2j])
-velocity = 3* np.array([1.25-1.25j,1.5-1.5j, 0.75-1.9j])
+position = 0.75*np.array([2+3j, 3+3.8j, 2+2j])
+velocity = 3* np.array([1.25-1.25j,1.5-1j, 0.5-1.9j])
+colours = ["yellow","violet","red"]
 collidedwithwallalready = np.array([False] * len(position))
-m = 0.5
-u = 0.02
+m = 500
+u = 0#0.025
 g = 9.81
-a = 0.5
+a = 0.025
 e=0.5
 xy_bound = 5
 res = 100
 px = 1/96
 
 dt=0.1
-fps=int( 1/dt )
+fps=int( 2/dt )
 
 z = lambda x,y : np.log(a + x**2 + y**2)
 dz_dx = lambda x,y : 2*x / (a + x**2 + y**2)
@@ -32,18 +33,10 @@ X,Y = np.meshgrid(x,y)
 Z = z(X,Y)
 Z = masked_where(X**2 + Y**2 > xy_bound**2, Z)
 
-theta = np.linspace(0, 2 * np.pi, res)
-z_c = np.linspace(Z.min(), Z.max() + 0.25, 100)
-theta, z_c = np.meshgrid(theta, z_c)
-
-x_c = xy_bound*np.cos(theta)
-y_c = xy_bound*np.sin(theta)
-
-def calculate_physics():
+def calculate_motion():
     global position, velocity
     
-    xs = position.real
-    ys = position.imag
+    xs,ys = position.real, position.imag
     
     zx_gradient = dz_dx(xs,ys)
     zy_gradient = dz_dy(xs,ys)
@@ -73,43 +66,74 @@ def calculate_physics():
     position += velocity*dt
     
 
+def calculate_wallphysics():
+    
+    global position, velocity, collidedwithwallalready, collision_scatters
+    
+    xs,ys = position.real,position.imag
+    u_x,u_y = velocity.real, velocity.imag
+    
+    collided_with_wall = xs ** 2 + ys ** 2 >= xy_bound ** 2
+
+
+    collidedwithwallalready[collidedwithwallalready & ~collided_with_wall] = False
+    just_collided = collided_with_wall & ~collidedwithwallalready
+    if np.any(just_collided):
+        collision_scatters.remove()
+         
+        x,y = position[just_collided].real,position[just_collided].imag
+        u_x,u_y = velocity[just_collided].real, velocity[just_collided].imag
+        
+        gradient = -x/y
+        theta = np.arctan(gradient)
+        
+        angle = 2*np.pi - theta
+        
+        rotated_u_x, rotated_u_y = [u_x * np.cos(angle) - u_y * np.sin(angle),
+                                    u_x * np.sin(angle) + u_y * np.cos(angle) ]
+        
+        
+        rotated_u_y *= -e
+        
+        v_x, v_y = [rotated_u_x * np.cos(theta) - rotated_u_y * np.sin(theta),
+                    rotated_u_x * np.sin(theta) + rotated_u_y * np.cos(theta) ]
+    
+    
+        velocity[just_collided] = v_x + 1j * v_y
+        
+        collision_scatters = ax.scatter(x,y,z(x,y), c="red",s=32,marker="x")
+    
+    collidedwithwallalready[just_collided] = True
+
+def create_cylinder():
+    theta = np.linspace(0, 2 * np.pi, res)
+    z_c = np.linspace(Z.min()-3, Z.max() + 0.25, 100)
+    theta, z_c = np.meshgrid(theta, z_c)
+
+    x_c = xy_bound*np.cos(theta)
+    y_c = xy_bound*np.sin(theta)
+    
+    # Plot the surface of the cylinder
+    ax.plot_surface(x_c, y_c, z_c, color="green", alpha=0.45)
 
 ax.invert_xaxis()
 ax.set_xlim([-xy_bound, xy_bound])
 ax.set_ylim(-xy_bound,xy_bound)
 
-# Plot the surface of the cylinder
-ax.plot_surface(x_c, y_c, z_c, color="green", alpha=0.45)
+create_cylinder()
 
 ax.plot_surface(X,Y,Z, color="blue", alpha=0.65)
 
 scattering = ax.scatter([],[],[], s=50, c="green", zorder=10)
+collision_scatters = ax.scatter([],[],[])
+    
 
-def calculate_wallphysics(position, v):
-    
-    
-    x,y = position.real,position.imag
-    u_x,u_y = v.real, v.imag
-    
-    gradient = -x/y
-    theta = np.arctan(gradient)
-    
-    angle = 2*np.pi - theta
-    
-    rotated_u_x, rotated_u_y = [u_x * np.cos(angle) - u_y * np.sin(angle),
-                                u_x * np.sin(angle) + u_y * np.cos(angle) ]
-    
-    rotated_u_y *= -e
-    
-    v_x, v_y = [rotated_u_x * np.cos(theta) - rotated_u_y * np.sin(theta),
-                rotated_u_x * np.sin(theta) + rotated_u_y * np.cos(theta) ]
-    
-    return v_x + 1j * v_y
 
 def animate(t):
-    global position, velocity, scattering, collidedwithwallalready
+    global position, velocity, scattering, collidedwithwallalready, collision_scatters
     
-    calculate_physics()
+    calculate_motion()
+    calculate_wallphysics()
     
     
     new_xs = position.real
@@ -117,20 +141,11 @@ def animate(t):
     
     scattering.remove()
     
-    collided_with_wall = new_xs ** 2 + new_ys ** 2 >= xy_bound ** 2
-
-
-    collidedwithwallalready[collidedwithwallalready & ~collided_with_wall] = False
-    just_collided = collided_with_wall & ~collidedwithwallalready
-    if np.any(just_collided):
-        velocity[just_collided] = calculate_wallphysics(position[just_collided], velocity[just_collided])
-        print("COLLIDED: " , just_collided)
-    collidedwithwallalready[just_collided] = True
     
     ax.set_zlim(Z.min()-0.5, Z.max() + 1 )
     
     new_zs= z(new_xs,new_ys)
-    scattering = ax.scatter(new_xs,new_ys,new_zs + 0.1, s=50, c="red", zorder=10)
+    scattering = ax.scatter(new_xs,new_ys,new_zs + 0.1, s=50, c=colours, zorder=10)
     
     
     
